034c7f3248a02332bb54925ebaf766270b87bf8b
ArsenKoch <arsenkocaran@icloud.com>
2022-06-14 19:28:57 +0300

    fixed homeworkApp

--- app/src/main/java/com/arsenka/movieshomeworkapp/MainActivity.kt
+++ app/src/main/java/com/arsenka/movieshomeworkapp/MainActivity.kt
@@ -7,9 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //TODO фонт(скачать и добавить в проект во вьюхи поставить)
-        // 2) сделать BACK (вью)
-        // 3) доделать каст
-        // 4) обернуть все в скролл вью
     }
 }
