13d19112c5542e9f6a5f91f47aa7cebf95153e63
ArsenKoch <arsenkocaran@icloud.com>
2022-07-12 20:49:32 +0300

    Homework5 doing please

--- app/src/main/java/com/arsenka/movieshomeworkapp/MainActivity.kt
+++ app/src/main/java/com/arsenka/movieshomeworkapp/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.arsenka.movieshomeworkapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +9,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+//TODO(изучить ВАЖНО):
+// 1) разобраться с сериализацией(термин,библиотека kotlin.serialization),
+// 2) разобраться с загрузкой картинок по URL(библиотека Glide) ,
+// 3) разобраться как передавать объект с одного фрагмента на другой.
+//TODO(примечания):
+// 1) калькулятор для рейтинга(округляем в меньшую сторону*),
+// 2) если нет актеров , то появляется надпись : "Здесь нет актеров"
+
